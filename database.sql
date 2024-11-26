dbee5435940319db918fd07c08c82282c7d6a628
MaDHouSe <madhouse1979@gmail.com>
2024-11-26 02:18:51 +0100

    ..

--- database.sql
+++
@@ -1,7 +0,0 @@
-
-CREATE TABLE IF NOT EXISTS `player_walkstyle` (
-  `id` int(10) NOT NULL AUTO_INCREMENT,
-  `license` varchar(255) NOT NULL,
-  `walkstyle` varchar(255) NOT NULL DEFAULT 'move_m@multiplayer',
-  PRIMARY KEY (`id`) USING BTREE
-) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci ROW_FORMAT=DYNAMIC;
--- server/main.lua
+++ server/main.lua
@@ -20,3 +20,15 @@ RegisterNetEvent("mh-walkstyle:server:onjoin", function()
     local result = MySQL.Sync.fetchScalar('SELECT walkstyle FROM player_walkstyle WHERE license = ? LIMIT 1', {license})
     if result then TriggerClientEvent('mh-walkstyle:client:update', src, result) end
 end)
+
+Citizen.CreateThread(function()
+    Wait(5100)
+    MySQL.Async.execute([[
+        CREATE TABLE IF NOT EXISTS `player_walkstyle` (
+            `id` int(10) NOT NULL AUTO_INCREMENT,
+            `license` varchar(255) NOT NULL,
+            `walkstyle` varchar(255) NOT NULL DEFAULT 'move_m@multiplayer',
+            PRIMARY KEY (`id`) USING BTREE
+        ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci ROW_FORMAT=DYNAMIC;  
+    ]])
+end)
