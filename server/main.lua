cbe165f13468bed9181971c56e928e02772e47d1
MaDHouSe <madhouse1979@gmail.com>
2024-11-26 03:00:25 +0100

    ...

--- server/main.lua
+++ server/main.lua
@@ -4,12 +4,12 @@
 RegisterNetEvent("mh-walkstyle:server:saveWalkStyle", function(style)
     local src = source
     local license = GetPlayerIdentifierByType(src, 'license')
-    local result = MySQL.Sync.fetchScalar('SELECT walkstyle FROM player_walkstyle WHERE license = ? LIMIT 1', {license})
+    local result = MySQL.Sync.fetchScalar('SELECT walkstyle FROM players WHERE license = ? LIMIT 1', {license})
     if result ~= nil and style then
-        MySQL.Async.execute('UPDATE player_walkstyle SET walkstyle = ? WHERE license = ?', {style, license})
+        MySQL.Async.execute('UPDATE players SET walkstyle = ? WHERE license = ?', {style, license})
         TriggerClientEvent('mh-walkstyle:client:update', src, style)
     elseif result == nil then
-        MySQL.Async.execute("INSERT INTO player_walkstyle (license, walkstyle) VALUES (?, ?)", {license, Config.DefaultWalkStyle})
+        MySQL.Async.execute("INSERT INTO players (license, walkstyle) VALUES (?, ?)", {license, Config.DefaultWalkStyle})
         TriggerClientEvent('mh-walkstyle:client:update', src, Config.DefaultWalkStyle)
     end
 end)
@@ -17,18 +17,11 @@ end)
 RegisterNetEvent("mh-walkstyle:server:onjoin", function()
     local src = source
     local license = GetPlayerIdentifierByType(src, 'license')
-    local result = MySQL.Sync.fetchScalar('SELECT walkstyle FROM player_walkstyle WHERE license = ? LIMIT 1', {license})
+    local result = MySQL.Sync.fetchScalar('SELECT walkstyle FROM players WHERE license = ? LIMIT 1', {license})
     if result then TriggerClientEvent('mh-walkstyle:client:update', src, result) end
 end)
 
-Citizen.CreateThread(function()
+CreateThread(function()
     Wait(5100)
-    MySQL.Async.execute([[
-        CREATE TABLE IF NOT EXISTS `player_walkstyle` (
-            `id` int(10) NOT NULL AUTO_INCREMENT,
-            `license` varchar(255) NOT NULL,
-            `walkstyle` varchar(255) NOT NULL DEFAULT 'move_m@multiplayer',
-            PRIMARY KEY (`id`) USING BTREE
-        ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci ROW_FORMAT=DYNAMIC;  
-    ]])
+    MySQL.Async.execute('ALTER TABLE players ADD COLUMN IF NOT EXISTS walkstyle VARCHAR(255) NOT NULL DEFAULT move_m@multiplayer')
 end)
