b96d45b7e736a5c9a49358016cc9d9db8f295734
MaDHouSe <madhouse1979@gmail.com>
2025-08-30 16:22:23 +0200

    upload
    ...

---
+++ fxmanifest.lua
@@ -0,0 +1,26 @@
+--[[ ===================================================== ]] --
+--[[            MH Walk Style Script by MaDHouSe           ]] --
+--[[ ===================================================== ]] --
+fx_version 'cerulean'
+games {'gta5'}
+
+author 'MaDHouSe'
+description 'MH - Walk Styles'
+version '1.0.1'
+
+shared_scripts {
+    'config.lua',
+}
+
+client_scripts {
+    '@ox_lib/init.lua',
+    'client/main.lua'
+}
+
+server_scripts {
+    '@oxmysql/lib/MySQL.lua',
+    'server/main.lua', 
+    'server/update.lua'
+}
+
+lua54 'yes'
